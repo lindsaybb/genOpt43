34c9a2698d5a3445018422147d72911fc915ecf9
LindsayBB <78917008+lindsaybb@users.noreply.github.com>
2021-11-03 09:32:22 -0400

    added decode

--- main.go
+++ main.go
@@ -12,12 +12,17 @@ import (
 
 var (
 	helpFlag = flag.Bool("h", false, "Show this help")
-	url string
+	val string
 )
 
 const usage = `'getOpt43' <url>
-This program encodes an ACS URL string to a HEX format able to be received by Innbox CPE and applied to their ACS client.
-Input format must begin with http and end with the port number, for example http://10.5.5.32:7547 or https://172.17.1.7:10302`
+This program encodes/decodes an ACS URL string to/from HEX.
+The format is able to be received by Innbox CPE and applied to their ACS client.
+Encode input must begin with http and end with the port number:
+\texample http://10.5.100.205:7547 or https://acs.lab.local:10302
+Decode input must begin with a HEX identifier 0x01:
+\texample 0x0118687474703a2f2f31302e352e3130302e3230353a37353437 
+\tor 0x011b68747470733a2f2f6163732e6c61622e6c6f63616c3a3130333032`
 
 func main() {
 	flag.Parse()
@@ -27,30 +32,42 @@ func main() {
 		return
 	}
 	if flag.NArg() < 1 {
-		fmt.Print("Supply ACS URL in format protocol://hostname[ip]:port\n>> ")
-		url = readFromStdin()
+		fmt.Print("Supply ACS URL in format protocol://hostname[ip]:port\nOr encoded value to reverse\n>> ")
+		val = readFromStdin()
 	} else {
 		// drop all trailing args
-		url = flag.Args()[0]
+		val = flag.Args()[0]
 	}
-	if !strings.HasPrefix(url, "http") {
+	if strings.HasPrefix(val, "http") {
+		// encode
+		tmp := strings.Split(val, ":")
+		if len(tmp) != 3 {
+			fmt.Println(usage)
+			return
+		}
+		src := []byte(val)
+		enc := hex.EncodeToString(src)
+		num := (len(enc)) / 2
+		// format:
+		// Hex Identifier and Option Code: 0x01
+		// Hex value of Data Length without Hex Identifier
+		// Hex value of ACS server URL
+		out := fmt.Sprintf("0x01%x%s", num, enc)
+		fmt.Println(out)
+	} else if strings.HasPrefix(val, "0x01") {
+		// decode
+		// num := url[4:5] // data length in hex
+		dec, err := hex.DecodeString(val[6:])
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(string(dec))
+		}
+	} else {
 		fmt.Println(usage)
 		return
 	}
-	tmp := strings.Split(url, ":")
-	if len(tmp) != 3 {
-		fmt.Println(usage)
-		return
-	}
-	src := []byte(url)
-	enc := hex.EncodeToString(src)
-	num := (len(enc)) / 2
-  // format:
-  //  Hex Identifier and Option Code: 0x01
-  //  Hex value of Data Length without Hex Identifier
-  //  Hex value of ACS server URL
-	out := fmt.Sprintf("0x01%x%s", num, enc)
-	fmt.Println(out)
+  
 }
 
 func readFromStdin() string {
